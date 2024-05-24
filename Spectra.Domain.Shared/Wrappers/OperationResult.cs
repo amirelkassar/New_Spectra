e4a1be48ecd2873ac23e72bd095fa953466959f6
amir elkassar <amir.elkassar@gmail.com>
2024-05-25 00:29:12 +0300

    OperationResult feature added

---
+++ Spectra.Domain.Shared/Wrappers/OperationResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectra.Domain.Shared.Wrappers
+{
+    public class OperationResult<TData>
+    {
+        protected OperationResult()
+        {
+            OperationId = Guid.NewGuid();
+            Errors = new Dictionary<string, string[]>();
+        }
+        protected OperationResult(TData data, int code, string message, IDictionary<string, string[]>? errors = null) : this()
+        {
+            Data = data;
+            Code = code;
+            Message = message;
+            Errors = errors;
+        }
+        public Guid OperationId { get; }
+
+        public int Code { get; set; }
+        public string? Message { get; set; }
+        public IDictionary<string, string[]>? Errors { get; set; }
+        public TData Data { get; set; }
+
+        public static OperationResult<TData> Sucess(TData data, int code = 200, string message = "Valid Operation!") => new OperationResult<TData>(data, code, message);
+        public static OperationResult<object?> Failure(IDictionary<string, string[]> errors, int code = 400, string message = "Invalid Operation!") => new OperationResult<object?>(null, code, message, errors);
+
+    }
+}
