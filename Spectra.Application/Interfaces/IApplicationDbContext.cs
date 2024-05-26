83970e91e1085ff3237126d46cdc5b69a744e1e3
adeln <adeln@NOHA_ELSHORBAGY>
2024-05-26 15:43:12 +0300

    Application Context Implementation

---
+++ Spectra.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectra.Application.Interfaces
+{
+	public interface IApplicationDbContext
+	{
+		Task<TEntity> GetById<TEntity>(string id) where TEntity : class;
+		Task<TEntity> Add<TEntity>(TEntity entity) where TEntity : class;
+		Task<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
+		Task Delete<TEntity>(TEntity entity) where TEntity : class;
+	}
+}
--- Spectra.Infrastructure/Data/ApplicationDbContext.cs
+++ Spectra.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Spectra.Application.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,37 @@ using System.Threading.Tasks;
 
 namespace Spectra.Infrastructure.Data
 {
-	public class ApplicationDbContext : DbContext
+	public class ApplicationDbContext : DbContext , IApplicationDbContext
 	{
 		public ApplicationDbContext(DbContextOptions options) : base(options)
 		{
 
 		}
+
+		public async Task<TEntity> GetById<TEntity>(string id) where TEntity : class
+		{
+			return await Set<TEntity>().FindAsync(id);
+		}
+
+		public async Task<TEntity> Add<TEntity>(TEntity entity) where TEntity : class
+		{
+			await Set<TEntity>().AddAsync(entity);
+			await SaveChangesAsync();
+			return entity;
+		}
+
+		public async Task<TEntity> Update<TEntity>(TEntity entity) where TEntity : class
+		{
+			Set<TEntity>().Update(entity);
+			await SaveChangesAsync();
+			return entity;
+		}
+
+		public async Task Delete<TEntity>(TEntity entity) where TEntity : class
+		{
+			Set<TEntity>().Remove(entity);
+			await SaveChangesAsync();
+		}
+
 	}
 }
