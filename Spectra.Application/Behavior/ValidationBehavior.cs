842edb717b199437a81af08c927dac9b3147b161
amir elkassar <amir.elkassar@gmail.com>
2024-05-30 22:53:21 +0300

    check any method replaced by count prop in validation behavior failuers condition

--- Spectra.Application/Behavior/ValidationBehavior.cs
+++ Spectra.Application/Behavior/ValidationBehavior.cs
@@ -18,7 +18,9 @@ namespace Spectra.Application.Common
 			_validators = validators;
 		}
 
-		public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+		public async Task<TResponse> Handle(TRequest request,
+			RequestHandlerDelegate<TResponse> next,
+			CancellationToken cancellationToken)
 		{
 			var context = new ValidationContext<TRequest>(request);
 
@@ -28,7 +30,7 @@ namespace Spectra.Application.Common
 				.Where(f => f != null)
 				.ToList();
 
-			if (failures.Any())
+			if (failures.Count != 0)
 			{
 				throw new Exceptions.ValidationException(failures);
 			}
