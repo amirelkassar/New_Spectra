6acc3476c56d61cb0799f4bdb6d323dec20e31ff
Omar Taha <omar.taha@corporate-mg.com>
2024-06-11 18:59:15 +0300

    requests edits

--- Spectra.Web/ClientApp/spectra-client/src/app/(dashboard)/requests/[requestId]/page.js
+++ Spectra.Web/ClientApp/spectra-client/src/app/(dashboard)/requests/[requestId]/page.js
@@ -23,30 +23,37 @@ const RequestDetailsPage = () => {
   const sala7eya = ["طبيب", "متخصص", "محاسب", "سكرتير"];
 
   return (
-    <div className="default-page text-xl ">
+    <div className="default-page text-xl space-y-2 ">
       <div>
         <h2 className="mb-1.5">{data.name}</h2>
         <h2>{data.spec.map((item) => item)}</h2>
       </div>
       <p className="max-w-[800px]">{data.desc}</p>
       <p className="max-w-[800px]">{data.title}</p>
-      <p className="text-base font-bold">التخصصات الدقيقة</p>
-      <div className="max-w-[800px] flex flex-wrap gap-x-5 gap-y-2">
+      <h2>التخصصات الدقيقة</h2>
+      <div className="max-w-[850px] flex flex-wrap gap-x-5 gap-y-2">
         {data.daqeqa.map((item, index) => (
-          <div key={index}>{item}</div>
+          <div key={index} className="bg-grayLight px-3 py-1 rounded-md">
+            {item}
+          </div>
         ))}
       </div>
       <h2>إضافة صلاحية</h2>
       <div className="flex gap-5">
         {sala7eya.map((item, index) => (
           <Button
-            className={"!rounded-full !py-2"}
+            className={"!rounded-2xl !py-1"}
             variant={data.spec.includes(item) ? "secondary" : "ternary"}
             key={index}
           >
             {item}
           </Button>
         ))}
+      </div>
+      <div className="grid grid-cols-3 gap-8 max-w-[850px]">
+        <Button variant="secondary">قبول</Button>
+        <Button>تعديل</Button>
+        <Button>رفض</Button>
       </div>
     </div>
   );
--- Spectra.Web/ClientApp/spectra-client/tailwind.config.js
+++ Spectra.Web/ClientApp/spectra-client/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         green: "#B3D9E2",
         greenLight: "#D7F0F6",
         blueLight: "#F1FCFF",
+        grayLight: "#F5F5F5",
         gray: "#F1F1F1",
         grayMedium: "#D9D9D9",
         grayDark: "#939393",
