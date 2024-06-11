db976e3210c66b7d1ec43e4b1dbeb9096bda08af
amir elkassar <amir.elkassar@gmail.com>
2024-06-11 11:26:30 +0300

    query base added

--- Spectra.Application/Messaging/ICommandBase.cs
+++ Spectra.Application/Messaging/ICommandBase.cs
@@ -10,10 +10,23 @@ namespace Spectra.Application.Messaging
 	public interface ICommand :IRequest,ICommandBase
 	{
 	}
-	public interface ICommand<TResponse>: IRequest<TResponse?>, ICommandBase
+
+	public interface IAuthorizedCommand : IRequest, ICommandBase
+	{
+
+	}
+
+    public interface ICommand<TResponse>: IRequest<TResponse?>, ICommandBase
 	{
 	}
-	public interface ICommandBase
+    public interface IAuthorizedCommand<TResponse> : ICommand<TResponse>
+    {
+
+    }
+
+    public interface ICommandBase
 	{
 	}
+
+
 }
---
+++ Spectra.Application/Messaging/IQueryBase.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectra.Application.Messaging
+{
+    public interface IQuery : IRequest, IQueryBase
+    {
+    }
+
+    public interface IAuthorizedQuery : IRequest, IQueryBase
+    {
+
+    }
+
+    public interface IQuery<TResponse> : IRequest<TResponse?>, IQueryBase
+    {
+    }
+    public interface IAuthorizedQuery<TResponse> : IQuery<TResponse>
+    {
+
+    }
+    public interface IQueryBase
+    {
+    }
+}
