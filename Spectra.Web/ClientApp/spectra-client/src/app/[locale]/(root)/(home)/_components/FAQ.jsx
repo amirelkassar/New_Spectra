0ed573f1399270a06554fb7d72829917f0b113c6
moo3tazali <moo3tazali@gmail.com>
2024-08-07 17:55:51 +0300

    FAQ section initialized

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(root)/(home)/_components/FAQ.jsx
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(root)/(home)/_components/FAQ.jsx
@@ -1,4 +1,42 @@
 import { Section } from '../../_components/section';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionControl,
+  AccordionPanel,
+} from '@mantine/core';
+
+const FAQData = [
+  {
+    id: 1,
+    label: 'طرق الدفع',
+    content:
+      'سبيكترا هو أول مركز طب إتصالي و رعاية عن بعد، يقوم بتشخيص و علاج و رعاية إضطرابات تطور و سلوك الاطفال، عن طريق فرقنا المتعددة التخصصات المختارة بعناية، وفق جودة و معايير فنية و مهنية عالمية .',
+  },
+  {
+    id: 2,
+    label: 'كيف استطيع عمل جلسة الكشف المبكر؟',
+    content: '',
+  },
+  {
+    id: 3,
+    label:
+      'ماهو فريق التشخيص متعدد التخصصات، Multidisciplinary team (MDT) ؟',
+    content: '',
+  },
+  {
+    id: 4,
+    label:
+      'هل علي الحضور في مكان معين للحصول على الخدمة؟ و ماهي التجهيزات المطلوبة مني ؟',
+    content: '',
+  },
+  {
+    id: 5,
+    label: 'ما مدى السرية التي سأحظى بها؟',
+    content: '',
+  },
+];
+
 export const FAQ = () => {
   return (
     <Section
@@ -8,7 +46,12 @@ export const FAQ = () => {
       heading='أسئلة شائعة'
       type='basic'
     >
-      FAQ
+      <Accordion radius='md'>
+        <AccordionItem value='item'>
+          <AccordionControl>control</AccordionControl>
+          <AccordionPanel>content</AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Section>
   );
 };
