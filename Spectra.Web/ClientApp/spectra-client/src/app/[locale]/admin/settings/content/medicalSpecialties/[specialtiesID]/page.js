1750e9de058cc05f23759d4566f45be19f0d3451
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-08-14 15:47:31 +0300

    handel select doctor in content

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/admin/settings/content/medicalSpecialties/[specialtiesID]/page.js
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/admin/settings/content/medicalSpecialties/[specialtiesID]/page.js
@@ -1,9 +1,13 @@
-'use client'
+"use client";
 import { columns } from "../_components/columns";
 import Button from "@/components/button";
 import Card from "@/components/card";
 import { DataTable } from "@/components/data-table";
-import React from "react";
+import { Checkbox, Table } from "@mantine/core";
+import Image from "next/image";
+import avatar from "@/assets/images/placeholder-person.png";
+import React, { useState } from "react";
+import StarGoldIcon from "@/assets/icons/starGold";
 const data = [
   {
     id: 0,
@@ -13,6 +17,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 5,
   },
   {
@@ -23,6 +28,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 5,
   },
   {
@@ -33,6 +39,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 4,
   },
   {
@@ -43,6 +50,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 3,
   },
   {
@@ -53,6 +61,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 3,
   },
   {
@@ -63,6 +72,7 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
+    avatar: avatar,
     star: 3,
   },
   {
@@ -73,20 +83,35 @@ const data = [
     specialisationDoctor: " اخصائى نفسى",
     date: "25/4/2024",
     time: "10:00 pm",
-    star: 3,
-  },
-  {
-    id: 7,
-    name: "عبدالله الشيخ",
-    kinshipName: "عائلة طفل",
-    doctor: "احمد محمد كمال",
-    specialisationDoctor: " اخصائى نفسى",
-    date: "25/4/2024",
-    time: "10:00 pm",
+    avatar: avatar,
     star: 3,
   },
 ];
-function page() {
+function Page() {
+  const [selectedUsers, setSelectedUsers] = useState(new Set());
+
+  const toggleUser = (index) => {
+    const newSelectedUsers = new Set(selectedUsers);
+    if (newSelectedUsers.has(index)) {
+      newSelectedUsers.delete(index);
+    } else {
+      if (selectedUsers.size >= 4) {
+        return null;
+      } else {
+        newSelectedUsers.add(index);
+      }
+    }
+    setSelectedUsers(newSelectedUsers);
+  };
+  const getStar = (num) => {
+    const stars = [];
+    for (let i = 1; i <= num; i++) {
+      stars.push(
+        <StarGoldIcon key={i} className={"w-[14px] lg:w-[18px] h-auto"} />
+      );
+    }
+    return stars;
+  };
   return (
     <Card>
       <h1 className="text-center my-4 mdl:my-8 text-[18px] mdl:text-[24px] font-Bold">
@@ -97,7 +122,51 @@ function page() {
         المطلوب وسحبه إلى المكان الجديد.
       </p>
       <div>
-        <DataTable  data={data} columns={columns} />
+        <Table>
+          <Table.Thead className="bg-blueLight h-[50px] min-h-[50px]  ">
+            <tr>
+              <Table.Th className="text-start text-xs font-ExtraLight lg:text-base rounded-s-xl ps-5">
+                الاسم
+              </Table.Th>
+              <Table.Th className="text-start text-xs font-ExtraLight lg:text-base">
+                تخصص الطبيب
+              </Table.Th>
+              <Table.Th className="text-start text-xs font-ExtraLight lg:text-base">
+                تاريخ الانضمام
+              </Table.Th>
+              <Table.Th className="text-start text-xs font-ExtraLight lg:text-base rounded-e-xl pe-5">
+                التقييم
+              </Table.Th>
+            </tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {data.map((doctor, index) => (
+              <Table.Tr key={index} className="mt-3">
+                <Table.Td className="flex items-center gap-4">
+                  <Checkbox
+                    size="xs"
+                    checked={selectedUsers.has(index)}
+                    onChange={() => toggleUser(index)}
+                  />
+                  <div className="flex items-center gap-2">
+                    <Image
+                      src={doctor.avatar}
+                      className=" size-[60px] rounded-full object-cover object-top"
+                    />
+                    <h2 className="text-base">{doctor.name}</h2>
+                  </div>
+                </Table.Td>
+                <Table.Td>{doctor.specialisationDoctor}</Table.Td>
+                <Table.Td>{doctor.date}</Table.Td>
+                <Table.Td>
+                  <div className="flex gap-[6px] items-center justify-start w-[116px]">
+                    {getStar(doctor.star)}
+                  </div>
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
       </div>
       <Button
         variant={"secondary"}
@@ -105,9 +174,8 @@ function page() {
       >
         تأكيد
       </Button>
-      
     </Card>
   );
 }
 
-export default page;
+export default Page;
