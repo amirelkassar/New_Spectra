d543ac43da2a552d6f0d748a47f2e0705d0be457
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-09-02 13:03:30 +0300

    add package + solve problems

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(client)/client/reports/[reportId]/_components/full-report.jsx
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(client)/client/reports/[reportId]/_components/full-report.jsx
@@ -14,8 +14,8 @@ export const FullReport = ({ id = '', title = '', content = '' }) => {
       <span className='text-xs lg:text-base block font-bold'>تقرير:{id}</span>
 
       <p className='text-xs lg:text-base lg:!leading-8'>
-        {content.split('.').map((i) => (
-          <span>
+        {content.split('.').map((i,j) => (
+          <span key={j}>
             {i}. <br />
           </span>
         ))}
