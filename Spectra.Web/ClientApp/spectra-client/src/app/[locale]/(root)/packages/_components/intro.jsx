6433182f3c54bd1c37f5e2a7d8fdb9eab846c20e
Moataz Ali <moo3tazali@gmail.com>
2024-11-06 17:05:23 +0200

    fix guest packages page style

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(root)/packages/_components/intro.jsx
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(root)/packages/_components/intro.jsx
@@ -8,9 +8,9 @@ export const Intro = () => {
       <div
         style={{
           clipPath:
-            'polygon(50% 100%, 100% 80%, 100% 0, 0 0, 0 80%)',
+            'polygon(50% 100%, 100% 85%, 100% 0, 0 0, 0 85%)',
         }}
-        className='bg-blueLight h-[50vh] pt-24 mdl:pt-32'
+        className='bg-blueLight h-[60vh] mdl:h-[55vh] pt-24 mdl:pt-28'
       >
         <h2
           id='all-packages'
@@ -25,7 +25,7 @@ export const Intro = () => {
         aria-label='All Packages'
         id='all-packages'
         aria-labelledby='all-packages'
-        className='flex flex-wrap justify-center gap-5 p-5 -mt-72 relative'
+        className='flex flex-col lg:flex-row items-center lg:items-start justify-center gap-5 p-5 mdl:-mt-72 -mt-96 relative'
       >
         {packagesDataSpectra.map((item) => (
           <PackageCard key={item.id} {...item} />
@@ -41,13 +41,13 @@ const PackageCard = ({
   content = [],
 }) => {
   return (
-    <PackageCardItem className='bg-white w-full max-w-sm first:border-t-transparent'>
+    <PackageCardItem className='bg-white w-full max-w-[300px] border-t-transparent border-2 border-greenMain rounded-2xl first:border-4 first:border-grayLight first:border-t-greenMain first:-mt-5 first:lg:order-2 last:lg:order-3 first:hover:border-grayLight first:hover:border-t-greenMain group'>
       <PackageCardItem.Title>{label}</PackageCardItem.Title>
       <PackageCardItem.Price>
         ${price}
       </PackageCardItem.Price>
       <PackageCardItem.List features={content} />
-      <PackageCardItem.Button>
+      <PackageCardItem.Button className='border border-greenMain hover:bg-transparent bg-transparent text-greenMain group-first:bg-greenMain group-first:text-white group-first:hover:bg-greenMain/90'>
         احجز الان
       </PackageCardItem.Button>
     </PackageCardItem>
