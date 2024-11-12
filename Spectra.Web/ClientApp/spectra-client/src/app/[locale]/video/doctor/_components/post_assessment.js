5663c72d7315343ff35957cab3e1a2f05aa9846e
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-11-12 10:59:12 +0200

    add one report

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/video/doctor/_components/post_assessment.js
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/video/doctor/_components/post_assessment.js
@@ -1,5 +1,7 @@
 import { Accordion } from "@/components/accordion";
+import Button from "@/components/button";
 import Levels from "@/components/levels";
+import { SurveyForm } from "@/components/swyc/survey-form";
 import React, { useState } from "react";
 
 // Component to display the level based on score ranges
@@ -7,14 +9,14 @@ function LevelDisplay({ score }) {
   const getLevelView = (score) => {
     if (score >= 0 && score <= 2) {
       return (
-        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white rounded-xl justify-center px-2 min-h-8">
+        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white shadow-md rounded-xl justify-center px-2 min-h-8">
           <span className={` size-4 min-w-4 rounded-full block bg-red`}></span>
           <p>Severe</p>
         </div>
       );
     } else if (score > 2 && score <= 4) {
       return (
-        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white rounded-xl justify-center px-2 min-h-8">
+        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white shadow-md rounded-xl justify-center px-2 min-h-8">
           <span
             className={` size-4 min-w-4 rounded-full block bg-purple`}
           ></span>{" "}
@@ -23,7 +25,7 @@ function LevelDisplay({ score }) {
       );
     } else if (score > 4 && score <= 7) {
       return (
-        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white rounded-xl justify-center px-2 min-h-8">
+        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white shadow-md rounded-xl justify-center px-2 min-h-8">
           <span
             className={` size-4 min-w-4 rounded-full block bg-black`}
           ></span>
@@ -32,7 +34,7 @@ function LevelDisplay({ score }) {
       );
     } else if (score > 7 && score <= 10) {
       return (
-        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white rounded-xl justify-center px-2 min-h-8">
+        <div className="flex items-center gap-2 mdl:gap-3 text-xs mdl:text-base font-Regular bg-white shadow-md rounded-xl justify-center px-2 min-h-8">
           <span
             className={` size-4 min-w-4 rounded-full block bg-greenMain`}
           ></span>
@@ -48,6 +50,15 @@ function LevelDisplay({ score }) {
 }
 
 function PostAssessment() {
+  const [answers, setAnswers] = useState({});
+
+  const handleSelect = (questionId, value) => {
+    const updatedAnswers = {
+      ...answers,
+      [questionId]: value,
+    };
+    setAnswers(updatedAnswers);
+  };
   const [Impression, setImpression] = useState([
     { id: 1, name: "ADLs", score: 0, showState: false },
     { id: 2, name: "Attention", score: 0, showState: false },
@@ -69,14 +80,11 @@ function PostAssessment() {
       )
     );
   };
-  const ViewCompState = () => {
-    switch (numLevel) {
-    }
-  };
+
   return (
     <div>
       <div dir="ltr">
-        <Accordion classNames={{ content: "px-0 " }} >
+        <Accordion classNames={{ content: "px-0 " }}>
           <Accordion.Item value={"Impression/ level of delay"}>
             <Accordion.Label>Impression/ level of delay</Accordion.Label>
             <Accordion.Content>
@@ -87,7 +95,7 @@ function PostAssessment() {
                     className="text-xs px-2 lg:px-4 odd:bg-grayDark/10 flex-wrap flex gap-x-5 items-center gap-y-2 mdl:gap-y-3 mdl:gap-x-10 mdl:text-base py-4 border-b border-grayLight last-of-type:border-none"
                     key={idx}
                   >
-                    <div className="flex items-center gap-2 flex-wrap mdl:flex-nowrap">
+                    <div className="flex items-center gap-2 flex-wrap ">
                       <h3 className=" text-xs mdl:text-base min-w-[174px]">
                         {item.name}
                       </h3>
@@ -110,7 +118,62 @@ function PostAssessment() {
             </Accordion.Content>
           </Accordion.Item>
         </Accordion>
+        <SurveyForm>
+          <SurveyForm.Body className="border-none">
+            <SurveyForm.Title className="border-none text-sm mdl:text-base">
+              Willingness of parents to get the intervention/ rehabilitation
+              sessions
+            </SurveyForm.Title>
+
+            <ul>
+              <SurveyForm.QuestionLi className="border-none before:hidden px-0 ps-0 ">
+                <SurveyForm.Question className="border-none">
+                  <SurveyForm.Answers>
+                    <SurveyForm.SingleAnswer
+                      name={`not welling`}
+                      value="0"
+                      checked={answers === 0}
+                      onChange={() => setAnswers(0)}
+                    >
+                      not welling
+                    </SurveyForm.SingleAnswer>
+                    <SurveyForm.SingleAnswer
+                      name={`low`}
+                      value="1"
+                      checked={answers === 1}
+                      onChange={() => setAnswers(1)}
+                    >
+                      low
+                    </SurveyForm.SingleAnswer>
+                    <SurveyForm.SingleAnswer
+                      name={`average`}
+                      value="2"
+                      checked={answers === 2}
+                      onChange={() => setAnswers(2)}
+                    >
+                      average
+                    </SurveyForm.SingleAnswer>
+                    <SurveyForm.SingleAnswer
+                      name={` high welling`}
+                      value="1"
+                      checked={answers === 3}
+                      onChange={() => setAnswers(3)}
+                    >
+                      high welling
+                    </SurveyForm.SingleAnswer>
+                  </SurveyForm.Answers>
+                </SurveyForm.Question>
+              </SurveyForm.QuestionLi>
+            </ul>
+          </SurveyForm.Body>
+        </SurveyForm>
       </div>
+      <Button
+        variant="secondary"
+        className="max-w-[294px] w-full mx-auto mb-3 mt-10"
+      >
+        حفظ
+      </Button>
     </div>
   );
 }
