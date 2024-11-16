66646eeee1c238be4311cce64731f0e15f53607f
amir elkassar <amir@profound-group.com>
2024-11-16 16:51:40 +0000

    Updated addReport.js

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/video/doctor/_components/addReport.js
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/video/doctor/_components/addReport.js
@@ -4,18 +4,27 @@ import { useSearchParams } from "next/navigation";
 import PostAssessment from "./reports/post_assessment";
 import FollowUp from "./reports/follow_up";
 import History from "./reports/history";
+import Speech from "./reports/speech";
+import MDT from "./reports/mdt";
+import Psychological from "./reports/psychological";
 
 function AddReport() {
   const searchParams = useSearchParams();
-
+ 
   const ViewCompReports = () => {
     switch (searchParams.get("reportNum")) {
       case "report1":
         return <PostAssessment />;
       case "report2":
         return <FollowUp />;
-        case "report3":
-          return <History />;
+      case "report3":
+        return <History />;
+      case "report4":
+        return <Speech />;
+      case "report5":
+        return <MDT />;
+      case "report6":
+        return <Psychological />;
     }
   };
   return <ViewCompReports />;
