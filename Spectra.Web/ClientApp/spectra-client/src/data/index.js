38c3d495ff6a8a4377a59b3da01692415b28d356
Moataz Ali <moo3tazali@gmail.com>
2024-11-16 23:58:46 +0200

    fix

--- Spectra.Web/ClientApp/spectra-client/src/data/index.js
+++ Spectra.Web/ClientApp/spectra-client/src/data/index.js
@@ -27,7 +27,7 @@ import { MOBILE_APP } from './mobile-app';
 import { SOCIAL } from './contact';
 import { REMINDER_MESSAGES } from './reminder-messages';
 import { CHILDS, CHILD_TABS, CHILDS_TABLE } from './child';
-// import { FAMILIES } from './families';
+import { FAMILIES } from './families';
 import { SETTINGS_LIST } from './settings';
 import { CLIENT_VIDEO_NAV } from './video';
 
@@ -59,7 +59,7 @@ export {
   CHILDS,
   CHILD_TABS,
   CHILDS_TABLE,
-  // FAMILIES,
+  FAMILIES,
   SETTINGS_LIST,
   CLIENT_VIDEO_NAV,
 };
