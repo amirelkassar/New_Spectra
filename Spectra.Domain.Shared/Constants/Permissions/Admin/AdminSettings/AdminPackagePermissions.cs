c373018ea8f725c5feef73d58e8b178cc0c5de52
amir elkassar <amir.elkassar@gmail.com>
2024-11-20 20:33:35 +0200

    Permissoin Classes updates

--- Spectra.Domain.Shared/Constants/Permissions/Admin/AdminSettings/AdminPackagePermissions.cs
+++ Spectra.Domain.Shared/Constants/Permissions/Admin/AdminSettings/AdminPackagePermissions.cs
@@ -9,9 +9,9 @@ namespace Spectra.Domain.Shared.Constants.Permissions.Admin.AdminSettings
 {
     public class AdminPackagePermissions : IPermissionContributor
     {
-        public const string Group = nameof(AdminPackagePermissions);
+        public const string Group = nameof(AdminPackagePermissions) + "Group.";
         //Crud operations permissoins
-        public const string CRUDPermissions = Group + nameof(CRUDPermissions);
+        public const string CRUDPermissions = Group + nameof(CRUDPermissions) + "Category";
         public const string Create = CRUDPermissions + nameof(Create);
         public const string Update = CRUDPermissions + nameof(Update);
         public const string Delete = CRUDPermissions + nameof(Delete);
--- Spectra.Domain.Shared/Constants/Permissions/Admin/AdminSettings/AdminSuccessStoryPermissions.cs
+++ Spectra.Domain.Shared/Constants/Permissions/Admin/AdminSettings/AdminSuccessStoryPermissions.cs
@@ -9,9 +9,9 @@ namespace Spectra.Domain.Shared.Constants.Permissions.Admin.AdminSettings
 {
     public class AdminSuccessStoryPermissions : IPermissionContributor
     {
-        public const string Group = nameof(AdminSuccessStoryPermissions);
+        public const string Group = nameof(AdminSuccessStoryPermissions) + "Group.";
         //Crud operations permissoins
-        public const string CRUDPermissions = Group + nameof(CRUDPermissions);
+        public const string CRUDPermissions = Group + nameof(CRUDPermissions) + "Category";
         public const string Create = CRUDPermissions + nameof(Create);
         public const string Update = CRUDPermissions + nameof(Update);
         public const string Delete = CRUDPermissions + nameof(Delete);
