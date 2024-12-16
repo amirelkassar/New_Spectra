8d76c525f419d6ad2496b24cde75fc6a73ca561f
amir.elkassar@gmail.com <amir.elkassar@gmail.com>
2024-12-17 01:07:08 +0200

    mongo db filter extensions added

---
+++ Spectra.Application/Commons/MongoDbFilterExtensions.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+using System.Linq.Expressions;
+
+public static class MongoFilterExtensions
+{
+    public static FilterDefinition<T> AddFilter<T>(this FilterDefinition<T> filter, Expression<Func<T, bool>> condition)
+    {
+        var filterBuilder = Builders<T>.Filter;
+        return filter & filterBuilder.Where(condition);
+    }
+
+    public static FilterDefinition<T> AddFilter<T>(this FilterDefinition<T> filter, bool applyFilter, Expression<Func<T, bool>> condition)
+    {
+        if (applyFilter)
+        {
+            var filterBuilder = Builders<T>.Filter;
+            return filter & filterBuilder.Where(condition);
+        }
+        return filter;
+    }
+
+    public static FilterDefinition<T> AddRegexFilter<T>(this FilterDefinition<T> filter, bool applyFilter, FieldDefinition<T> field, string pattern, string options = "i")
+    {
+        if (applyFilter)
+        {
+            var filterBuilder = Builders<T>.Filter;
+            var regexFilter = filterBuilder.Regex(field, new MongoDB.Bson.BsonRegularExpression(pattern, options));
+            return filter & regexFilter;
+        }
+        return filter;
+    }
+
+}
