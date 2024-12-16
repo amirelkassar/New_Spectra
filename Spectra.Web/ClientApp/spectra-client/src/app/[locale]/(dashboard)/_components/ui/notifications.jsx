c5fd6cf1c78d03506e1f42ad520af27806bfc9a1
Moataz Ali <moo3tazali@gmail.com>
2024-12-16 18:07:42 +0200

    update notifications to support make as read and redirects

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(dashboard)/_components/ui/notifications.jsx
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(dashboard)/_components/ui/notifications.jsx
@@ -1,60 +1,88 @@
 'use client';
 
+import { useCallback, useMemo, useState } from 'react';
 import { Popover } from '@mantine/core';
 import { Divider } from '@mantine/core';
-import { Link } from '@/i18n/routing';
+import { Link, useRouter } from '@/i18n/routing';
 import { Notification } from '@mantine/core';
 
-import ROUTES from '@/routes';
 import NotificationIcon from '@/assets/icons/notification';
-import { useNotifications } from '@/hooks/queries/user/notifications';
+import {
+  useMakeNotificationRead,
+  useNotifications,
+} from '@/hooks/queries/user/notifications';
 import { QueryWrapper } from '@/components/query-wrapper';
 import { useDate } from '@/hooks/use-date';
+import ROUTES from '@/routes';
 
 export const Notifications = () => {
+  const router = useRouter();
+
+  const [opened, setOpened] = useState(false);
+
   const query = useNotifications();
+
+  const { mutate: makeNotificationRead } = useMakeNotificationRead();
+
+  const onNotificationClick = useCallback(
+    (notification) => {
+      makeNotificationRead(notification?.id);
+      if (notification?.objectUrl) {
+        router.push(notification.objectUrl);
+        setOpened(false);
+      }
+    },
+    [makeNotificationRead, router]
+  );
+
+  const dropdownContent = useMemo(() => {
+    return (
+      <div className='min-w-[calc(100vw-26px)] h-[400px] overflow-y-auto mdl:min-w-[650px] flex flex-col p-4'>
+        <div className='flex-1'>
+          <QueryWrapper query={query}>
+            {({ data }) =>
+              data.map((notification) => (
+                <NotificationItem
+                  onClick={() => onNotificationClick(notification)}
+                  key={notification.id}
+                  {...notification}
+                />
+              ))
+            }
+          </QueryWrapper>
+        </div>
+
+        <Link
+          className='block text-end text-sm mdl:text-base font-bold text-greenMain hover:underline'
+          href={ROUTES.CLIENT.NOTIFICATIONS}
+        >
+          عرض الكل
+        </Link>
+      </div>
+    );
+  }, [query, onNotificationClick]);
 
   return (
     <Popover
+      opened={opened}
+      onChange={setOpened}
       position='bottom-end'
       clickOutsideEvents={['mouseup', 'touchend']}
       offset={10}
       classNames={{
-        dropdown: 'rounded-xl border-grayLight shadow-md',
+        dropdown: 'rounded-xl border-grayLight shadow-md p-0',
       }}
     >
       <Popover.Target>
-        <button className='shrink-0 p-0 size-9 mdl:size-11 rounded-full bg-blueLight flex items-center justify-center'>
+        <button
+          onClick={() => setOpened((o) => !o)}
+          className='shrink-0 p-0 size-9 mdl:size-11 rounded-full bg-blueLight flex items-center justify-center'
+        >
           <NotificationIcon className='size-4 mdl:size-5' />
         </button>
       </Popover.Target>
 
-      <Popover.Dropdown>
-        <div className='min-w-[calc(100vw-67px)] h-96 overflow-y-auto mdl:min-w-[650px] flex flex-col'>
-          <div className='flex-1'>
-            <QueryWrapper query={query}>
-              {({ data }) =>
-                data.map((notification) => (
-                  <NotificationItem
-                    onClick={() => {
-                      // console.log(notification.id);
-                    }}
-                    key={notification.id}
-                    {...notification}
-                  />
-                ))
-              }
-            </QueryWrapper>
-          </div>
-
-          <Link
-            className='block text-end text-sm mdl:text-base font-bold text-greenMain hover:underline'
-            href={ROUTES.CLIENT.NOTIFICATIONS}
-          >
-            عرض الكل
-          </Link>
-        </div>
-      </Popover.Dropdown>
+      <Popover.Dropdown>{dropdownContent}</Popover.Dropdown>
     </Popover>
   );
 };
@@ -74,7 +102,11 @@ const NotificationItem = ({
     <>
       <Notification
         radius={3}
-        onClick={onClick}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          onClick(e);
+        }}
         classNames={{
           root: 'shadow-none cursor-pointer transition hover:bg-blueLighter',
           description: 'flex items-center gap-4',
