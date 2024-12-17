5df329ce417d4c2f193cdfc2370c1633fbeee0e8
Moataz Ali <moo3tazali@gmail.com>
2024-12-17 13:44:43 +0200

    style fixes

--- Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(dashboard)/admin/(routes)/main-data/layout.jsx
+++ Spectra.Web/ClientApp/spectra-client/src/app/[locale]/(dashboard)/admin/(routes)/main-data/layout.jsx
@@ -1,10 +1,11 @@
+import Card from '@/components/card';
 import MainDataAside from './_components/main-data-aside';
 
 export default function MainDataLayout({ children }) {
   return (
     <div className='flex flex-col lg:flex-row gap-5 h-full'>
       <MainDataAside />
-      <div className='flex-1 overflow-hidden'>{children}</div>
+      <Card className='flex-1 overflow-hidden !p-0'>{children}</Card>
     </div>
   );
 }
