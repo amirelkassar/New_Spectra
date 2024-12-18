cb6a0ba710dfcb40d461367cc7d71aead11294a2
amir.elkassar@gmail.com <amir.elkassar@gmail.com>
2024-12-18 20:06:44 +0200

    old chats servics deleted

--- Spectra.Application/Chats/Services/IChatService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Spectra.Application.ChatHub.Services
-{
-    public interface IChatService
-    {
-        Task BroadcastMessageAsync(string fromUser, string message);
-        Task SendMessageAsync(string fromUser, string toUser, string message);
-    }
-}
