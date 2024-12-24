fd64eb4af23b1dca27a9481d9fc712303f376666
amir.elkassar@gmail.com <amir.elkassar@gmail.com>
2024-12-25 00:28:17 +0200

    get chat rooms queries added

---
+++ Spectra.Application/Chats/Dtos/MessageReadDto.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using Spectra.Domain.Chats;
+using Spectra.Domain.Shared.Common;
+using Spectra.Domain.Shared.Enums;
+
+namespace Spectra.Application.Chats.Dtos
+{
+    public class MessageReadDto : BaseEntityDto<string>
+    {
+
+        public MessageType Type { get; set; }
+        public string ChatId { get; set; } 
+        public string SenderId { get; set; } 
+        public string? Content { get; set; }
+        public string? FileUrl { get; set; }
+    }
+}
