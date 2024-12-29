553a7e4aa5c22b25b36b21e46f5bb7d485338e2c
amir.elkassar@gmail.com <amir.elkassar@gmail.com>
2024-12-29 13:30:53 +0200

    employee apis added

---
+++ Spectra.WebAPI/Areas/EmployeeHead/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Spectra.Application.Employees.Queries;
+using Spectra.Application.Interfaces;
+
+namespace Spectra.WebAPI.Areas.EmployeeHead
+{
+    public class EmployeeController(ILogger<EmployeeController> logger, ICurrentUser currentUser, ISender sender) : EmployeeHeadControllerBase<EmployeeController>(logger, currentUser)
+    {
+        private readonly ISender _sender = sender;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] GetEmployeeForSectionHeadByIdQuery input)
+        {
+            var response = await _sender.Send(input);
+            return Ok(response);
+        }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> GetListAsync([FromQuery] GetEmployeeListByHeadDepartmentQuery input)
+        {
+            var response = await _sender.Send(input);
+            return Ok(response);
+        }
+    }
+}
