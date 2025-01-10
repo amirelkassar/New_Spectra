9b9af716a8cda9d7cb44ac6f3251bd1df81e2ff3
amir.elkassar@gmail.com <amir.elkassar@gmail.com>
2025-01-10 20:47:52 +0200

    old doctor schedual structure deleted

--- Spectra.Application/ScheduleAppointments/Appointments/DTO/AppointmentDetailDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Spectra.Application.ScheduleAppointments.Appointments.DTO
-{
-    public class AppointmentDetailDto
-    {
-        public DateTime DateOfAppointment { get; set; }
-        public TimeOnly TimeOfAppoinment { get; set; }
-
-    }
-}
--- Spectra.Application/ScheduleAppointments/DoctorSchedules/DTO/CreateDoctorScheduleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Spectra.Application.ScheduleAppointments.DoctorSchedules.Commands;
-
-namespace Spectra.Application.ScheduleAppointments.DoctorSchedules.DTO
-{
-    public class CreateDoctorScheduleDto
-    {
-        public List<CreateDoctorScheduleCommand> CreateDoctorScheduleCommands { get; set; }
-    }
-}
